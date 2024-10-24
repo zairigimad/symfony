9c5919942c0e7d04470fefc2a23815c5b9d46e5c
Mathieu Santostefano <msantostefano@protonmail.com>
2024-10-24 14:06:29 +0200

    Add webhooks signature verification on Sweego bridges

--- src/Symfony/Component/Notifier/Bridge/Sweego/Tests/Webhook/SweegoWrongSignatureRequestParserTest.php
+++ src/Symfony/Component/Notifier/Bridge/Sweego/Tests/Webhook/SweegoWrongSignatureRequestParserTest.php
@@ -9,28 +9,31 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Component\Mailer\Bridge\Sweego\Tests\Webhook;
+namespace Symfony\Component\Notifier\Bridge\Sweego\Tests\Webhook;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\Bridge\Sweego\RemoteEvent\SweegoPayloadConverter;
-use Symfony\Component\Mailer\Bridge\Sweego\Webhook\SweegoRequestParser;
+use Symfony\Component\Notifier\Bridge\Sweego\Webhook\SweegoRequestParser;
 use Symfony\Component\Webhook\Client\RequestParserInterface;
+use Symfony\Component\Webhook\Exception\RejectWebhookException;
 use Symfony\Component\Webhook\Test\AbstractRequestParserTestCase;
 
-class SweegoRequestParserTest extends AbstractRequestParserTestCase
+class SweegoWrongSignatureRequestParserTest extends AbstractRequestParserTestCase
 {
     protected function createRequestParser(): RequestParserInterface
     {
-        return new SweegoRequestParser(new SweegoPayloadConverter());
+        $this->expectException(RejectWebhookException::class);
+        $this->expectExceptionMessage('Invalid signature.');
+
+        return new SweegoRequestParser();
     }
 
     protected function createRequest(string $payload): Request
     {
         return Request::create('/', 'POST', [], [], [], [
             'Content-Type' => 'application/json',
-            'HTTP_webhook-id' => '9f26b9d0-13d7-410c-ba04-5019cd30e6d0',
-            'HTTP_webhook-timestamp' => '1723737959',
-            'HTTP_webhook-signature' => 'W+fm4VPshCGjuT0HxyV00QEbFitZd2Rdvx82bWM7VXc=',
+            'HTTP_webhook-id' => 'a5ccc627-6e43-4012-bb29-f1bfe3a3d13e',
+            'HTTP_webhook-timestamp' => '1725290740',
+            'HTTP_webhook-signature' => 'wrong_signature',
         ], $payload);
     }
 }
