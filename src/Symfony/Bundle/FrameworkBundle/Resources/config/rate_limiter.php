619c0eb1800f0ae49e7cb42f0c6e6c696fafbcfd
Alexandre Daubois <alex.daubois@gmail.com>
2024-11-20 11:25:30 +0100

    [RateLimiter] Add `RateLimiterFactoryInterface`

--- src/Symfony/Bundle/FrameworkBundle/Resources/config/rate_limiter.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/config/rate_limiter.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Symfony\Component\RateLimiter\RateLimiterFactory;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
@@ -27,4 +28,9 @@ return static function (ContainerConfigurator $container) {
                 null,
             ])
     ;
+
+    if (interface_exists(RateLimiterFactoryInterface::class)) {
+        $container->services()
+            ->alias(RateLimiterFactoryInterface::class, 'limiter');
+    }
 };
