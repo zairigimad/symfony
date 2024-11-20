596487b8ca5e7f754020934cbb7e052589f1c1d3
Nicolas Grekas <nicolas.grekas@gmail.com>
2024-11-20 10:52:39 +0100

    [FrameworkBundle] Don't auto-register form/csrf when the corresponding components are not installed

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/form_csrf_disabled.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/form_csrf_disabled.php
@@ -4,6 +4,7 @@ $container->loadFromExtension('framework', [
     'annotations' => false,
     'csrf_protection' => false,
     'form' => [
+        'enabled' => true,
         'csrf_protection' => true,
     ],
     'http_method_override' => false,
