d662f85c65b7f293a0d5389ebb7a3cac79c17924
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-06-02 13:57:50 +0200

    [JsonStreamer] Add `include_null_properties` option

--- src/Symfony/Component/JsonStreamer/Tests/Fixtures/stream_writer/null.php
+++ src/Symfony/Component/JsonStreamer/Tests/Fixtures/stream_writer/null.php
@@ -5,7 +5,7 @@
  */
 return static function (mixed $data, \Psr\Container\ContainerInterface $valueTransformers, array $options): \Traversable {
     try {
-        yield 'null';
+        yield "null";
     } catch (\JsonException $e) {
         throw new \Symfony\Component\JsonStreamer\Exception\NotEncodableValueException($e->getMessage(), 0, $e);
     }
